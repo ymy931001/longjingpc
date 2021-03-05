2cd7011e83518ea397d871d9e0c00c471f93ab76
ymy931001 <yumaoyang@terabits.cn>
2021-03-05 10:45:43 +0800

    3.5

--- src/axios/config.js
+++ src/axios/config.js
@@ -2,7 +2,7 @@
 //  export const requestIp = 'http://garden.longjing.terabits-wx.cn:4000'; //移动服务器
 //export const requestIp = "http://122.152.249.37:4000"; //测试服务器
 // export const requestIp = "http://garden.longjing.terabits-wx.cn:4000";
-export const requestIp = "http://118.31.71.163:4000";
+export const requestIp = "http://36.134.8.74:4000";
 export const requestIp2 = "http://garden.longjing.terabits-wx.cn:4001";
 
 export const downUrl = requestIp//下载报告地址
